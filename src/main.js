3be99346aea90e35607233af835c28871b2fa0d6
Danger <51889207+DangerDan88@users.noreply.github.com>
2020-03-08 17:53:41 -0600

    started to build a simple app wioth Vue js

---
+++ src/main.js
@@ -0,0 +1,9 @@
+var app = new Vue({
+  el: "#app",
+  data: {
+    product: "chicken and waffles",
+    image: "src/assets/chicken.jpg",
+    link: "https://www.roscoeschickenandwaffles.com/our-menu",
+    inventory: 10
+  }
+});
